08864742a57bd5040fc5c7b8e83cfd6bb1e3b204
offline <rojor3d@gmail.com>
2024-06-03 16:13:12 -0500

    test

--- 00-scripts.sh
+++ 00-scripts.sh
@@ -17,3 +17,508 @@ sudo -S apt update -y
 ~/openvas/13-redis.sh
 ~/openvas/14-permissions.sh
 ~/openvas/15-feed.sh
+
+### 01-prereqs ###
+
+#Setting an install prefix environment variable
+export INSTALL_PREFIX=/usr/local
+
+#Adjusting PATH for running gvmd
+export PATH=$PATH:$INSTALL_PREFIX/sbin
+
+#Choosing a source directory
+export SOURCE_DIR=$HOME/source
+mkdir -p $SOURCE_DIR
+
+#Choosing a build directory
+export BUILD_DIR=$HOME/build
+mkdir -p $BUILD_DIR
+
+#Choosing a temporary install directory
+export INSTALL_DIR=$HOME/install
+mkdir -p $INSTALL_DIR
+
+#Installing Common Build Dependencies
+sudo apt update
+sudo apt install --no-install-recommends --assume-yes \
+  build-essential \
+  curl \
+  cmake \
+  pkg-config \
+  python3 \
+  python3-pip \
+  gnupg
+  
+curl -f -L https://www.greenbone.net/GBCommunitySigningKey.asc -o /tmp/GBCommunitySigningKey.asc
+gpg --import /tmp/GBCommunitySigningKey.asc
+
+echo "8AE4BE429B60A59B311C2E739823FAA60ED1E580:6:" | gpg --import-ownertrust
+
+### 02-gvm-libs ###
+
+#Setting the gvm-libs version to use
+
+export GVM_LIBS_VERSION=22.8.0
+
+#Required dependencies for gvm-libs
+sudo apt install -y \
+  libglib2.0-dev \
+  libgpgme-dev \
+  libgnutls28-dev \
+  uuid-dev \
+  libssh-gcrypt-dev \
+  libhiredis-dev \
+  libxml2-dev \
+  libpcap-dev \
+  libnet1-dev \
+  libpaho-mqtt-dev
+  
+#Optional dependencies for gvm-libs
+sudo apt install -y \
+  libldap2-dev \
+  libradcli-dev
+  
+#Downloading the gvm-libs sources
+curl -f -L https://github.com/greenbone/gvm-libs/archive/refs/tags/v$GVM_LIBS_VERSION.tar.gz -o $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz
+curl -f -L https://github.com/greenbone/gvm-libs/releases/download/v$GVM_LIBS_VERSION/gvm-libs-v$GVM_LIBS_VERSION.tar.gz.asc -o $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz.asc
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz
+
+#Building gvm-libs
+mkdir -p $BUILD_DIR/gvm-libs && cd $BUILD_DIR/gvm-libs
+
+cmake $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DSYSCONFDIR=/etc \
+  -DLOCALSTATEDIR=/var
+
+make -j$(nproc)
+
+#Installing gvm-libs
+mkdir -p $INSTALL_DIR/gvm-libs
+
+make DESTDIR=$INSTALL_DIR/gvm-libs install
+
+sudo cp -rv $INSTALL_DIR/gvm-libs/* /
+
+### 03-gvmd ###
+
+#Setting the gvmd version to use
+export GVMD_VERSION=23.2.0
+
+#Required dependencies for gvmd
+sudo apt install -y \
+  libglib2.0-dev \
+  libgnutls28-dev \
+  libpq-dev \
+  postgresql-server-dev-14 \
+  libical-dev \
+  xsltproc \
+  rsync \
+  libbsd-dev \
+  libgpgme-dev
+  
+#Optional dependencies for gvmd
+sudo apt install -y --no-install-recommends \
+  texlive-latex-extra \
+  texlive-fonts-recommended \
+  xmlstarlet \
+  zip \
+  rpm \
+  fakeroot \
+  dpkg \
+  nsis \
+  gnupg \
+  gpgsm \
+  wget \
+  sshpass \
+  openssh-client \
+  socat \
+  snmp \
+  python3 \
+  smbclient \
+  python3-lxml \
+  gnutls-bin \
+  xml-twig-tools
+  
+#Downloading the gvmd sources
+curl -f -L https://github.com/greenbone/gvmd/archive/refs/tags/v$GVMD_VERSION.tar.gz -o $SOURCE_DIR/gvmd-$GVMD_VERSION.tar.gz
+curl -f -L https://github.com/greenbone/gvmd/releases/download/v$GVMD_VERSION/gvmd-$GVMD_VERSION.tar.gz.asc -o $SOURCE_DIR/gvmd-$GVMD_VERSION.tar.gz.asc
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/gvmd-$GVMD_VERSION.tar.gz
+
+#Building gvmd
+mkdir -p $BUILD_DIR/gvmd && cd $BUILD_DIR/gvmd
+
+cmake $SOURCE_DIR/gvmd-$GVMD_VERSION \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLOCALSTATEDIR=/var \
+  -DSYSCONFDIR=/etc \
+  -DGVM_DATA_DIR=/var \
+  -DGVMD_RUN_DIR=/run/gvmd \
+  -DOPENVAS_DEFAULT_SOCKET=/run/ospd/ospd-openvas.sock \
+  -DGVM_FEED_LOCK_PATH=/var/lib/gvm/feed-update.lock \
+  -DSYSTEMD_SERVICE_DIR=/lib/systemd/system \
+  -DLOGROTATE_DIR=/etc/logrotate.d
+
+make -j$(nproc)
+
+#Installing gvmd
+mkdir -p $INSTALL_DIR/gvmd
+
+make DESTDIR=$INSTALL_DIR/gvmd install
+
+sudo cp -rv $INSTALL_DIR/gvmd/* /
+
+### 04-pg-gvm ###
+
+export PG_GVM_VERSION=22.6.4
+
+#Required dependencies for pg-gvm
+sudo apt install -y \
+  libglib2.0-dev \
+  postgresql-server-dev-14 \
+  libical-dev
+  
+#Downloading the pg-gvm sources
+curl -f -L https://github.com/greenbone/pg-gvm/archive/refs/tags/v$PG_GVM_VERSION.tar.gz -o $SOURCE_DIR/pg-gvm-$PG_GVM_VERSION.tar.gz
+curl -f -L https://github.com/greenbone/pg-gvm/releases/download/v$PG_GVM_VERSION/pg-gvm-$PG_GVM_VERSION.tar.gz.asc -o $SOURCE_DIR/pg-gvm-$PG_GVM_VERSION.tar.gz.asc
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/pg-gvm-$PG_GVM_VERSION.tar.gz
+
+#Building pg-gvm
+mkdir -p $BUILD_DIR/pg-gvm && cd $BUILD_DIR/pg-gvm
+
+cmake $SOURCE_DIR/pg-gvm-$PG_GVM_VERSION \
+  -DCMAKE_BUILD_TYPE=Release
+
+make -j$(nproc)
+
+#Installing pg-gvm
+mkdir -p $INSTALL_DIR/pg-gvm
+
+make DESTDIR=$INSTALL_DIR/pg-gvm install
+
+sudo cp -rv $INSTALL_DIR/pg-gvm/* /
+
+### 05-gsa ###
+
+export GSA_VERSION=23.0.0
+
+#Downloading the gsa sources
+curl -f -L https://github.com/greenbone/gsa/releases/download/v$GSA_VERSION/gsa-dist-$GSA_VERSION.tar.gz -o $SOURCE_DIR/gsa-$GSA_VERSION.tar.gz
+curl -f -L https://github.com/greenbone/gsa/releases/download/v$GSA_VERSION/gsa-dist-$GSA_VERSION.tar.gz.asc -o $SOURCE_DIR/gsa-$GSA_VERSION.tar.gz.asc
+
+mkdir -p $SOURCE_DIR/gsa-$GSA_VERSION
+tar -C $SOURCE_DIR/gsa-$GSA_VERSION -xvzf $SOURCE_DIR/gsa-$GSA_VERSION.tar.gz
+
+#Installing gsa
+sudo mkdir -p $INSTALL_PREFIX/share/gvm/gsad/web/
+sudo cp -rv $SOURCE_DIR/gsa-$GSA_VERSION/* $INSTALL_PREFIX/share/gvm/gsad/web/
+
+### 06-gsad ###
+
+#Setting the GSAd version to use
+export GSAD_VERSION=22.9.0
+
+#Required dependencies for gsad
+sudo apt install -y \
+  libmicrohttpd-dev \
+  libxml2-dev \
+  libglib2.0-dev \
+  libgnutls28-dev
+  
+#Downloading the gsad sources
+curl -f -L https://github.com/greenbone/gsad/archive/refs/tags/v$GSAD_VERSION.tar.gz -o $SOURCE_DIR/gsad-$GSAD_VERSION.tar.gz
+curl -f -L https://github.com/greenbone/gsad/releases/download/v$GSAD_VERSION/gsad-$GSAD_VERSION.tar.gz.asc -o $SOURCE_DIR/gsad-$GSAD_VERSION.tar.gz.asc
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/gsad-$GSAD_VERSION.tar.gz
+
+#Building gsad
+mkdir -p $BUILD_DIR/gsad && cd $BUILD_DIR/gsad
+
+cmake $SOURCE_DIR/gsad-$GSAD_VERSION \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DSYSCONFDIR=/etc \
+  -DLOCALSTATEDIR=/var \
+  -DGVMD_RUN_DIR=/run/gvmd \
+  -DGSAD_RUN_DIR=/run/gsad \
+  -DLOGROTATE_DIR=/etc/logrotate.d
+
+make -j$(nproc)
+
+#Installing gsad
+mkdir -p $INSTALL_DIR/gsad
+
+make DESTDIR=$INSTALL_DIR/gsad install
+
+sudo cp -rv $INSTALL_DIR/gsad/* /
+
+#### 07-openvas-smb ###
+
+#Setting the openvas-smb version to use
+export OPENVAS_SMB_VERSION=22.5.3
+
+#Required dependencies for openvas-smb
+sudo apt install -y \
+  gcc-mingw-w64 \
+  libgnutls28-dev \
+  libglib2.0-dev \
+  libpopt-dev \
+  libunistring-dev \
+  heimdal-dev \
+  perl-base
+  
+#Downloading the openvas-smb sources
+curl -f -L https://github.com/greenbone/openvas-smb/archive/refs/tags/v$OPENVAS_SMB_VERSION.tar.gz -o $SOURCE_DIR/openvas-smb-$OPENVAS_SMB_VERSION.tar.gz
+curl -f -L https://github.com/greenbone/openvas-smb/releases/download/v$OPENVAS_SMB_VERSION/openvas-smb-v$OPENVAS_SMB_VERSION.tar.gz.asc -o $SOURCE_DIR/openvas-smb-$OPENVAS_SMB_VERSION.tar.gz.asc
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/openvas-smb-$OPENVAS_SMB_VERSION.tar.gz
+
+#Building openvas-smb
+mkdir -p $BUILD_DIR/openvas-smb && cd $BUILD_DIR/openvas-smb
+
+cmake $SOURCE_DIR/openvas-smb-$OPENVAS_SMB_VERSION \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DCMAKE_BUILD_TYPE=Release
+
+make -j$(nproc)
+
+#Installing openvas-smb
+mkdir -p $INSTALL_DIR/openvas-smb
+
+make DESTDIR=$INSTALL_DIR/openvas-smb install
+
+sudo cp -rv $INSTALL_DIR/openvas-smb/* /
+
+### 08-openvas-scanner ###
+
+### openvas-scanner
+export OPENVAS_SCANNER_VERSION=23.0.1
+
+#Required dependencies for openvas-scanner
+sudo apt install -y \
+  bison \
+  libglib2.0-dev \
+  libgnutls28-dev \
+  libgcrypt20-dev \
+  libpcap-dev \
+  libgpgme-dev \
+  libksba-dev \
+  rsync \
+  nmap \
+  libjson-glib-dev \
+  libcurl4-gnutls-dev \
+  libbsd-dev
+  
+#Debian optional dependencies for openvas-scanner
+sudo apt install -y \
+  python3-impacket \
+  libsnmp-dev
+  
+#Downloading the openvas-scanner sources
+curl -f -L https://github.com/greenbone/openvas-scanner/archive/refs/tags/v$OPENVAS_SCANNER_VERSION.tar.gz -o $SOURCE_DIR/openvas-scanner-$OPENVAS_SCANNER_VERSION.tar.gz
+curl -f -L https://github.com/greenbone/openvas-scanner/releases/download/v$OPENVAS_SCANNER_VERSION/openvas-scanner-v$OPENVAS_SCANNER_VERSION.tar.gz.asc -o $SOURCE_DIR/openvas-scanner-$OPENVAS_SCANNER_VERSION.tar.gz.asc
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/openvas-scanner-$OPENVAS_SCANNER_VERSION.tar.gz
+
+#Building openvas-scanner
+mkdir -p $BUILD_DIR/openvas-scanner && cd $BUILD_DIR/openvas-scanner
+
+cmake $SOURCE_DIR/openvas-scanner-$OPENVAS_SCANNER_VERSION \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DINSTALL_OLD_SYNC_SCRIPT=OFF \
+  -DSYSCONFDIR=/etc \
+  -DLOCALSTATEDIR=/var \
+  -DOPENVAS_FEED_LOCK_PATH=/var/lib/openvas/feed-update.lock \
+  -DOPENVAS_RUN_DIR=/run/ospd
+
+make -j$(nproc)
+
+#Installing openvas-scanner
+mkdir -p $INSTALL_DIR/openvas-scanner
+
+make DESTDIR=$INSTALL_DIR/openvas-scanner install
+
+sudo cp -rv $INSTALL_DIR/openvas-scanner/* /
+
+printf "table_driven_lsc = yes\n" | sudo tee /etc/openvas/openvas.conf
+sudo printf "openvasd_server = http://127.0.0.1:3000\n" | sudo tee -a /etc/openvas/openvas.conf
+
+### 09-ospd-openvas ###
+
+#Setting the ospd and ospd-openvas versions to use
+export OSPD_OPENVAS_VERSION=22.6.2
+
+#Required dependencies for ospd-openvas
+sudo apt install -y \
+  python3 \
+  python3-pip \
+  python3-setuptools \
+  python3-packaging \
+  python3-wrapt \
+  python3-cffi \
+  python3-psutil \
+  python3-lxml \
+  python3-defusedxml \
+  python3-paramiko \
+  python3-redis \
+  python3-gnupg \
+  python3-paho-mqtt
+  
+#Downloading the ospd-openvas sources
+curl -f -L https://github.com/greenbone/ospd-openvas/archive/refs/tags/v$OSPD_OPENVAS_VERSION.tar.gz -o $SOURCE_DIR/ospd-openvas-$OSPD_OPENVAS_VERSION.tar.gz
+curl -f -L https://github.com/greenbone/ospd-openvas/releases/download/v$OSPD_OPENVAS_VERSION/ospd-openvas-v$OSPD_OPENVAS_VERSION.tar.gz.asc -o $SOURCE_DIR/ospd-openvas-$OSPD_OPENVAS_VERSION.tar.gz.asc
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/ospd-openvas-$OSPD_OPENVAS_VERSION.tar.gz
+
+#Installing ospd-openvas
+cd $SOURCE_DIR/ospd-openvas-$OSPD_OPENVAS_VERSION
+
+mkdir -p $INSTALL_DIR/ospd-openvas
+
+python3 -m pip install --root=$INSTALL_DIR/ospd-openvas --no-warn-script-location .
+
+sudo cp -rv $INSTALL_DIR/ospd-openvas/* /
+
+### 10-openvasd ###
+
+#Setting the openvas versions to use
+export OPENVAS_DAEMON=23.0.1
+
+#Required dependencies for openvasd
+sudo apt install -y \
+  cargo \
+  pkg-config \
+  libssl-dev
+  
+#Downloading the openvas-scanner sources
+curl -f -L https://github.com/greenbone/openvas-scanner/archive/refs/tags/v$OPENVAS_DAEMON.tar.gz -o $SOURCE_DIR/openvas-scanner-$OPENVAS_DAEMON.tar.gz
+curl -f -L https://github.com/greenbone/openvas-scanner/releases/download/v$OPENVAS_DAEMON/openvas-scanner-v$OPENVAS_DAEMON.tar.gz.asc -o $SOURCE_DIR/openvas-scanner-$OPENVAS_DAEMON.tar.gz.asc
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/openvas-scanner-$OPENVAS_DAEMON.tar.gz
+
+mkdir -p $INSTALL_DIR/openvasd/usr/local/bin
+cd $SOURCE_DIR/openvas-scanner-$OPENVAS_DAEMON/rust/openvasd
+cargo build --release
+
+cd $SOURCE_DIR/openvas-scanner-$OPENVAS_DAEMON/rust/scannerctl
+cargo build --release
+
+sudo cp -v ../target/release/openvasd $INSTALL_DIR/openvasd/usr/local/bin/
+sudo cp -v ../target/release/scannerctl $INSTALL_DIR/openvasd/usr/local/bin/
+sudo cp -rv $INSTALL_DIR/openvasd/* /
+
+### 11-greenbone-feed-sync ###
+
+#Required dependencies for greenbone-feed-sync
+sudo apt install -y \
+  python3 \
+  python3-pip
+  
+#Installing greenbone-feed-sync system-wide for all users
+sudo apt install -y \
+  python3 \
+  python3-pip
+  
+#Installing greenbone-feed-sync system-wide for all users
+mkdir -p $INSTALL_DIR/greenbone-feed-sync
+
+python3 -m pip install --root=$INSTALL_DIR/greenbone-feed-sync --no-warn-script-location greenbone-feed-sync
+
+sudo cp -rv $INSTALL_DIR/greenbone-feed-sync/* /
+
+### 12-gvm-tools ###
+
+#Required dependencies for gvm-tools
+sudo apt install -y \
+  python3 \
+  python3-pip \
+  python3-venv \
+  python3-setuptools \
+  python3-packaging \
+  python3-lxml \
+  python3-defusedxml \
+  python3-paramiko
+  
+#Installing gvm-tools system-wide
+mkdir -p $INSTALL_DIR/gvm-tools
+
+python3 -m pip install --root=$INSTALL_DIR/gvm-tools --no-warn-script-location gvm-tools
+
+sudo cp -rv $INSTALL_DIR/gvm-tools/* /
+
+### 13-redis ###
+
+#Installing the Redis server
+sudo apt install -y redis-server
+
+#Adding configuration for running the Redis server for the scanner
+sudo cp $SOURCE_DIR/openvas-scanner-$OPENVAS_SCANNER_VERSION/config/redis-openvas.conf /etc/redis/
+sudo chown redis:redis /etc/redis/redis-openvas.conf
+echo "db_address = /run/redis-openvas/redis.sock" | sudo tee -a /etc/openvas/openvas.conf
+
+#Start redis with openvas config
+sudo systemctl start redis-server@openvas.service
+
+#Ensure redis with openvas config is started on every system startup
+sudo systemctl enable redis-server@openvas.service
+
+#Adding the gvm user to the redis group
+sudo usermod -aG redis gvm
+
+### 14-permissions ###
+
+#Adjusting directory permissions
+sudo mkdir -p /var/lib/notus
+sudo mkdir -p /run/gvmd
+
+sudo chown -R gvm:gvm /var/lib/gvm
+sudo chown -R gvm:gvm /var/lib/openvas
+sudo chown -R gvm:gvm /var/lib/notus
+sudo chown -R gvm:gvm /var/log/gvm
+sudo chown -R gvm:gvm /run/gvmd
+
+sudo chmod -R g+srw /var/lib/gvm
+sudo chmod -R g+srw /var/lib/openvas
+sudo chmod -R g+srw /var/log/gvm
+
+#Adjusting gvmd permissions
+sudo chown gvm:gvm /usr/local/sbin/gvmd
+sudo chmod 6750 /usr/local/sbin/gvmd
+
+### 15-feed ###
+
+#Creating a GPG keyring for feed content validation
+curl -f -L https://www.greenbone.net/GBCommunitySigningKey.asc -o /tmp/GBCommunitySigningKey.asc
+
+export GNUPGHOME=/tmp/openvas-gnupg
+mkdir -p $GNUPGHOME
+
+gpg --import /tmp/GBCommunitySigningKey.asc
+echo "8AE4BE429B60A59B311C2E739823FAA60ED1E580:6:" | gpg --import-ownertrust
+
+export OPENVAS_GNUPG_HOME=/etc/openvas/gnupg
+sudo mkdir -p $OPENVAS_GNUPG_HOME
+sudo cp -r /tmp/openvas-gnupg/* $OPENVAS_GNUPG_HOME/
+sudo chown -R gvm:gvm $OPENVAS_GNUPG_HOME
+
+### 16-sudoscan ###
+
+# Create a temporary file to hold the new sudoers content
+TEMP_FILE=$(mktemp)
+
+# Backup the original sudoers file
+sudo cp /etc/sudoers /etc/sudoers.bak
+
+# Add the new lines to the temporary file
+echo "%gvm ALL = NOPASSWD: /usr/local/sbin/openvas" > "$TEMP_FILE"
+
+sudo cat "$TEMP_FILE" >> /etc/sudoers
+
+# Clean up
+sudo rm "$TEMP_FILE"
